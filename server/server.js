32d974946c7cc6198d7cb87d32b3f86fb3e964bf
SUHO PARK <duglikespiano@gmail.com>
2025-12-28 13:29:13 +0900

    SERVER - BASIC EXPRESS SERVER ADDED

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import 'dotenv/config.js';
+import express from 'express';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+
+const app = express();
+
+//Security Middleware
+app.use(helmet());
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+		credentials: true,
+	})
+);
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	message: 'Too many requests from this IP, please try agai nafter some time',
+});
+app.use(limiter);
+
+app.use(express.json({ limit: '10mb' }));
